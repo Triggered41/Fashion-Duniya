0e597fae346ae4e3820989e6b16adc4fabb2adac
Triggered41 <vehdathamid@gmail.com>
2022-06-20 09:14:31 +0530

    validaion

---
+++ JS/form_validation.js
@@ -0,0 +1,25 @@
+var form = document.forms['form'];
+
+form.onsubmit = function(e){
+  e.preventDefault();
+
+  if (form.pin != null && form.cpass != null && form.number != null){
+
+      if (!(/^\d+$/.test(form.pin.value))){
+          alert("Check you pincode");
+          return;
+        }
+        if (!(/^\d+$/.test(form.number.value))){
+            alert("Check you Number");
+            return;
+        }
+        
+        if (form.pass.value != form.cpass.value){
+            alert("passwords don't match")
+        }else{
+            alert("you have been registered")
+            form.submit()
+        }
+    }
+
+}
